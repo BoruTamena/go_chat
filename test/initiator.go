605efd92e686a067f80e89f356ec6e69b53563c2
BoruTamena <borutamena46@gmail.com>
2025-01-03 10:27:35 +0300

    test initiator added

--- test/initiator.go
+++ test/initiator.go
@@ -1,5 +1,13 @@
 package test
 
+import (
+	"log"
+	"os"
+
+	"github.com/BoruTamena/go_chat/initiator"
+	"github.com/gin-gonic/gin"
+)
+
 // your test initiator goes here
 
 type TestInstance struct {
@@ -12,6 +20,33 @@ type TestInstance struct {
 		PlatformLayer
 
 	*/
+	Sv       *gin.Engine
+	Handler  initiator.Handler
+	Moudle   initiator.Module
+	Platform initiator.PlatFormLayer
 }
 
-func InitiateTest(arg any) (TestInstance, any)
+func InitiateTest(arg string) TestInstance {
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Llongfile)
+
+	test_server := gin.Default()
+
+	v1 := test_server.Group("v1")
+
+	platform := initiator.InitPlatFormLayer()
+
+	module := initiator.InitModule(logger, platform)
+
+	handler := initiator.IntHandler(logger, module)
+
+	initiator.InitRouter(*v1, handler, module, platform)
+
+	return TestInstance{
+		Sv:       test_server,
+		Handler:  handler,
+		Moudle:   module,
+		Platform: platform,
+	}
+
+}
