1d3617da1563d26565e0ec5b851a65cee6b9e9e5
BoruTamena <borutamena46@gmail.com>
2025-01-17 17:12:28 +0300

    models defined for user

--- internal/constant/errors/error_types.go
+++ internal/constant/errors/error_types.go
@@ -10,19 +10,25 @@ var (
 )
 
 var (
-	ClientErr = errorx.NewType(WsError, "Socket Client Error:error")
-	CNotFound = errorx.NewType(WsError, "Socket Client Not Found:error ")
-
+	// Websocket errors
+	ClientErr      = errorx.NewType(WsError, "Socket Client Error:error")
+	CNotFound      = errorx.NewType(WsError, "Socket Client Not Found:error ")
 	WsConErr       = errorx.NewType(WsError, "Web SocketConnection:error")
 	WsReadErr      = errorx.NewType(WsError, "Web Socket Read:error")
 	WsUnRigsterErr = errorx.NewType(WsError, "Handler Not Regisetred:error")
 	RoomErr        = errorx.NewType(WsError, "Room:error")
-	BadInput       = errorx.NewType(InputError, "Bad user input:error")
-	MarshalErr     = errorx.NewType(InternalError, "unable to marshal:error")
-	UnMarshalErr   = errorx.NewType(InternalError, "unable to unmarshal:error")
 
+	// Internal server errors
+	BadInput     = errorx.NewType(InputError, "Bad user input:error")
+	MarshalErr   = errorx.NewType(InternalError, "unable to marshal:error")
+	UnMarshalErr = errorx.NewType(InternalError, "unable to unmarshal:error")
+	DublicateErr = errorx.NewType(InternalError, "Duplicate::error")
+	AuthErr      = errorx.NewType(InternalError, "Unauthorized user :: error")
+
+	// Database errors
 	WriteErr  = errorx.NewType(DbError, "db write :: error ")
 	NullObjId = errorx.NewType(DbError, "Null Object Id Returned :: error")
+	DbReadErr = errorx.NewType(DbError, "db read::error")
 )
 
 var (
--- internal/constant/models/persistencedb/mgdb.go
+++ internal/constant/models/persistencedb/mgdb.go
@@ -3,11 +3,13 @@ package persistencedb
 import (
 	"log"
 
+	"github.com/BoruTamena/go_chat/internal/constant/models/db"
 	"github.com/BoruTamena/go_chat/internal/constant/models/dto"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type MgPersistence struct {
+	db.Queries
 	client *mongo.Client
 	lg     *log.Logger
 	cfg    dto.Config
