b97d0f3b3f358064c0ce136a10f0fefa71485dd5
BoruTamena <borutamena46@gmail.com>
2025-01-17 17:11:32 +0300

    user storage layer added

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,12 +3,15 @@ package storage
 import (
 	"context"
 
+	"github.com/BoruTamena/go_chat/internal/constant/models/db"
 	"github.com/BoruTamena/go_chat/internal/constant/models/dto"
 )
 
 // define U storage interface here
 
 type User interface {
+	CreateUser(ctx context.Context, user dto.User) (db.User, error)
+	GetUserByEmail(ctx context.Context, email string) (db.User, error)
 }
 
 type Chat interface {
